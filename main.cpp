bf8cddf1bef20e69174b8670b46b0a440264b6c9
zwpwjwtz <zwpwjwtz@126.com>
2017-10-22 09:29:27 +0200

    Check Qt version before enabling HiDPI scaling

--- main.cpp
+++ main.cpp
@@ -5,7 +5,9 @@ int main(int argc, char *argv[])
 {
     // Enable scaling for HiDPI device
     qputenv("QT_AUTO_SCREEN_SCALE_FACTOR", "1");
+#if QT_VERSION >= QT_VERSION_CHECK(5, 6, 0)
     QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
 
     QApplication a(argc, argv);
     MainWindow w;
