aa91bdad7661eb3040cbe279023cff3f8b7c4881
zwpwjwtz <zwpwjwtz@126.com>
2017-12-02 20:30:44 +0100

    Enable HiDPI scaling only when QT_SCALE_FACTOR has not been set

--- main.cpp
+++ main.cpp
@@ -4,10 +4,13 @@
 int main(int argc, char *argv[])
 {
     // Enable scaling for HiDPI device
-    qputenv("QT_AUTO_SCREEN_SCALE_FACTOR", "1");
+    if (qgetenv("QT_SCALE_FACTOR").isEmpty())
+    {
+        qputenv("QT_AUTO_SCREEN_SCALE_FACTOR", "1");
 #if QT_VERSION >= QT_VERSION_CHECK(5, 6, 0)
-    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+        QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
+    }
 
     QApplication a(argc, argv);
     MainWindow w;
